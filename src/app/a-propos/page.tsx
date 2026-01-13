9605e7b054117549c6d7d77a22159b338ff0e987
Yaniss-Elie <yaniss123elie@gmail.com>
2026-01-13 18:38:12 +0100

    update

--- src/app/a-propos/page.tsx
+++ src/app/a-propos/page.tsx
@@ -8,22 +8,22 @@ const teamMembers = [
     {
         name: "Membre 1",
         role: "Président",
-        image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=300&h=300&fit=crop&crop=face",
+        image: "",
     },
     {
         name: "Membre 2",
         role: "Vice-Président",
-        image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=300&h=300&fit=crop&crop=face",
+        image: "",
     },
     {
         name: "Membre 3",
         role: "Secrétaire Général",
-        image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=300&h=300&fit=crop&crop=face",
+        image: "",
     },
     {
         name: "Membre 4",
         role: "Responsable Tech",
-        image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=300&h=300&fit=crop&crop=face",
+        image: "",
     },
 ];
 
